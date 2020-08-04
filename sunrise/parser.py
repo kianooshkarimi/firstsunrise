a67bf66ab29df9618a2ad939c8ea2d33e3def5c6
Kianoosh Karimi <kianoosh_k@yahoo.com>
2020-08-04 06:43:41 +0430

    Updated according to PEP8.

--- sunrise/parser.py
+++ sunrise/parser.py
@@ -2,9 +2,8 @@ import re
 
 from sunrise.actions import Action, Calculator
 
+
 # Importing the text entered by the user and checking if does it match to the calculation pattern or not.
-
-
 def parse(usertext):
     calculator_regex = r'[+-]? \s* \(? \s* [0-9]+ ([.][0-9]+)? \s* \)? \s* [\+ - \* \/] \s* \(? \s* [+-]? \s* [0-9]+ ([.][0-9]+)? \s* \)?'
     list_of_action_patterns = [
