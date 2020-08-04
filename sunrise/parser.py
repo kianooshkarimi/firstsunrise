d28b5f1405998001fe390443577cd0f3cfef5b7c
Kianoosh Karimi <kianoosh_k@yahoo.com>
2020-08-04 06:36:06 +0430

    Updated according to PEP8

--- sunrise/parser.py
+++ sunrise/parser.py
@@ -6,18 +6,18 @@ from sunrise.actions import Action, Calculator
 
 
 def parse(usertext):
-    calculator_RegEx = r'[+-]? \s* \(? \s* [0-9]+ ([.][0-9]+)? \s* \)? \s* [\+ - \* \/] \s* \(? \s* [+-]? \s* [0-9]+ ([.][0-9]+)? \s* \)?'
+    calculator_regex = r'[+-]? \s* \(? \s* [0-9]+ ([.][0-9]+)? \s* \)? \s* [\+ - \* \/] \s* \(? \s* [+-]? \s* [0-9]+ ([.][0-9]+)? \s* \)?'
     list_of_action_patterns = [
-            [calculator_RegEx, Calculator]
+            [calculator_regex, Calculator]
             ]
-    classification = ''       
+    classification = None       
     regex = ''
     output = None
 # Classifying the output of the function, if the input matched with the corresponding pattern.
     for i in range(len(list_of_action_patterns)): 
-        RegEx = list_of_action_patterns[i][0]
+        regex = list_of_action_patterns[i][0]
         classification = list_of_action_patterns[i][1]
-        matched_or_not = re.search(RegEx, usertext)
+        matched_or_not = re.search(regex, usertext)
         if (bool(matched_or_not)):
             output = classification
     return output
