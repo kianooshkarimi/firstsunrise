e9967946bf76ae90b5466edbce46f9d9c38118cd
Kianoosh Karimi <kianoosh_k@yahoo.com>
2020-08-09 13:17:47 +0430

    Calculation action recognition updated according to PEP8

---
+++ sunrise/actions.py
@@ -0,0 +1,9 @@
+class Action:
+    pass
+      
+
+class Calculator(Action):
+    def __init__(self,text):
+        self.text = text
+
+
---
+++ sunrise/parser.py
@@ -0,0 +1,25 @@
+import re
+
+from sunrise.actions import Action, Calculator
+
+
+# importing the text entered by the user and checking if does matchs to the calculation pattern or not
+def parse(usertext):
+    calculator_regex = r'[+-]?\s*\(?\s*[0-9]+([.][0-9]+)?\s*\)?\s*[\+ - \* \/]\s*\(?\s*[+-]?\s*[0-9]+([.][0-9]+)?\s*\)?'
+    list_of_action_patterns = [
+        [calculator_regex, Calculator(usertext)]
+            ]
+
+    clasfict = ''       
+    regex = ''
+    output = None
+    for i in range(len(list_of_action_patterns)):
+        regex = list_of_action_patterns[i][0]
+        #classifying the output of the function, as a Calculator (or any other action in future extentions)
+        clasfictn = list_of_action_patterns[i][1]
+        matched_or_not = re.search(regex, usertext)
+        if bool(matched_or_not):
+            output = clasfictn
+    return output
+
+
