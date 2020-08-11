656d7afb1c35352da29941fbbc7f667578f37b6a
Kianoosh Karimi <kianoosh_k@yahoo.com>
2020-08-11 13:01:30 +0430

    Calculation action recognition updated according to PEP8

--- sunrise/parser.py
+++ sunrise/parser.py
@@ -2,16 +2,17 @@ import re
 
 from sunrise.actions import Action, Calculator
 
+calculator_regex = r'[+-]?\s*\(?\s*[0-9]+([.][0-9]+)?\s*\)?\s*[\+ - \* \/]\s*\(?\s*[+-]?\s*[0-9]+([.][0-9]+)?\s*\)?'
+list_of_action_patterns = [
+        [calculator_regex, Calculator]
+        ]
+clasfict = ''
+regex = ''
+output = None
+
 
 # importing the text entered by the user and checking if does matchs to the calculation pattern or not
 def parse(usertext):
-    calculator_regex = r'[+-]?\s*\(?\s*[0-9]+([.][0-9]+)?\s*\)?\s*[\+ - \* \/]\s*\(?\s*[+-]?\s*[0-9]+([.][0-9]+)?\s*\)?'
-    list_of_action_patterns = [
-        [calculator_regex, Calculator(usertext)]
-            ]
-    clasfict = ''       
-    regex = ''
-    output = None
     for i in range(len(list_of_action_patterns)):
         regex = list_of_action_patterns[i][0]
         #classifying the output of the function, as a Calculator (or any other action in future extentions)
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -3,7 +3,14 @@ from sunrise.actions import Action, Calculator
 
 
 def test_parser():
-    action = parser.parse('2 + 4')
-    assert isinstance(action, Action)
-    assert isinstance(action, Calculator)
+    action = parser.parse('2.3 *(- 43.825)')
+    try:
+        assert issubclass(action, Action)
+    except:
+        print('Cannot parse the entered text as an action')
+        
+    try:
+        assert issubclass(action, Calculator)
+    except:
+        print('cannot parse the entered text as a calculation action')
 
